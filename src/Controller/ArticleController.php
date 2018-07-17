309c54504a7b9eef94c4dc9b57b6590225a5a574
Ivan Zykov <ivan.v.zykov@gmail.com>
2018-07-17 09:47:58 +0200

    After video 2

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -2,13 +2,29 @@
 
 namespace App\Controller;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Response;
 
 class ArticleController
 {
+	/**
+	* @Route("/")
+	*
+	**/
 	public function homepage()
 	{
 		return new Response ('OMG! My first page already!');
+	}
+
+	/**
+	*@Route("/news/{slug}	")
+	**/
+	public function show($slug)
+	{
+		return new Response(sprintf(
+			'Future way to show... %s',
+			$slug
+		));
 	}
 }
 
